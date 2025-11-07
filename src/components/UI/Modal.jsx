c9f1a55583dd1dbc40eb6469e43905700e7b99e9
jbwp <jarekb0605@outlook.com>
2025-11-07 08:04:39 +0100

    added cart modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,10 +1,11 @@
-import { Fragment } from 'react';
-import ReactDOM from 'react-dom';
+import { Fragment } from "react";
+// import ReactDOM from 'react-dom';
+import { createPortal } from "react-dom";
 
-import classes from './Modal.module.css';
+import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose}/>;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -15,13 +16,13 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById('overlays');
+const portalElement = document.getElementById("overlays");
 
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
-      {ReactDOM.createPortal(
+      {createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
